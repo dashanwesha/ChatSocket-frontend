b4f2a5fece494a1445ce07e486e93a75fb4de535
dashanwesha <22051927@kiit.ac.in>
2025-03-01 23:53:18 +0530

    Updated

--- src/lib/socketClient.ts
+++ src/lib/socketClient.ts
@@ -1,4 +1,4 @@
 "use client";
 
 import {io} from "socket.io-client";
-export const socket = io("https://chatsocket-q8do.onrender.com", { transports: ["websocket"] });
+export const socket = io("https://chatsocket-j5j5.onrender.com", { transports: ["websocket"] });
